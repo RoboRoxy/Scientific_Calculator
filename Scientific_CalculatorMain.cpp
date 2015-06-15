3184e4dde5e49a4d364824ed8d451ee1719c558d
RoboRoxy <dilaaaajaya@hotmail.com>
2015-06-15 20:29:37 +0530

    UI is done halfway

--- Scientific_CalculatorMain.cpp
+++ Scientific_CalculatorMain.cpp
@@ -42,6 +42,21 @@ wxString wxbuildinfo(wxbuildinfoformat format)
 }
 
 //(*IdInit(Scientific_CalculatorFrame)
+const long Scientific_CalculatorFrame::ID_TEXTCTRL1 = wxNewId();
+const long Scientific_CalculatorFrame::ID_PANEL1 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON1 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON2 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON3 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON4 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON5 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON6 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON7 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON8 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON9 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON10 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON11 = wxNewId();
+const long Scientific_CalculatorFrame::ID_BUTTON12 = wxNewId();
+const long Scientific_CalculatorFrame::ID_PANEL2 = wxNewId();
 const long Scientific_CalculatorFrame::idMenuQuit = wxNewId();
 const long Scientific_CalculatorFrame::idMenuAbout = wxNewId();
 const long Scientific_CalculatorFrame::ID_STATUSBAR1 = wxNewId();
@@ -56,12 +71,58 @@ Scientific_CalculatorFrame::Scientific_CalculatorFrame(wxWindow* parent,wxWindow
 {
     //(*Initialize(Scientific_CalculatorFrame)
     wxMenuItem* MenuItem2;
+    wxGridBagSizer* GridBagSizer1;
+    wxGridBagSizer* GridBagSizer2;
     wxMenuItem* MenuItem1;
     wxMenu* Menu1;
+    wxBoxSizer* BoxSizer1;
     wxMenuBar* MenuBar1;
     wxMenu* Menu2;
+    wxGridBagSizer* GridBagSizer3;
 
     Create(parent, id, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T("id"));
+    GridBagSizer1 = new wxGridBagSizer(0, 0);
+    Panel1 = new wxPanel(this, ID_PANEL1, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL1"));
+    BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
+    TextCtrl1 = new wxTextCtrl(Panel1, ID_TEXTCTRL1, wxEmptyString, wxDefaultPosition, wxSize(500,22), 0, wxDefaultValidator, _T("ID_TEXTCTRL1"));
+    BoxSizer1->Add(TextCtrl1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Panel1->SetSizer(BoxSizer1);
+    BoxSizer1->Fit(Panel1);
+    BoxSizer1->SetSizeHints(Panel1);
+    GridBagSizer1->Add(Panel1, wxGBPosition(0, 0), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    GridBagSizer2 = new wxGridBagSizer(0, 0);
+    Panel2 = new wxPanel(this, ID_PANEL2, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL2"));
+    GridBagSizer3 = new wxGridBagSizer(0, 0);
+    Button1 = new wxButton(Panel2, ID_BUTTON1, _("X!"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON1"));
+    GridBagSizer3->Add(Button1, wxGBPosition(0, 0), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button2 = new wxButton(Panel2, ID_BUTTON2, _("("), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON2"));
+    GridBagSizer3->Add(Button2, wxGBPosition(0, 1), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button3 = new wxButton(Panel2, ID_BUTTON3, _(")"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON3"));
+    GridBagSizer3->Add(Button3, wxGBPosition(0, 2), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button4 = new wxButton(Panel2, ID_BUTTON4, _("%"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON4"));
+    GridBagSizer3->Add(Button4, wxGBPosition(0, 3), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button5 = new wxButton(Panel2, ID_BUTTON5, _("AC"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON5"));
+    GridBagSizer3->Add(Button5, wxGBPosition(0, 4), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button6 = new wxButton(Panel2, ID_BUTTON6, _("Inv"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON6"));
+    GridBagSizer3->Add(Button6, wxGBPosition(1, 0), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button7 = new wxButton(Panel2, ID_BUTTON7, _("sin"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON7"));
+    GridBagSizer3->Add(Button7, wxGBPosition(1, 1), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button8 = new wxButton(Panel2, ID_BUTTON8, _("In"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON8"));
+    GridBagSizer3->Add(Button8, wxGBPosition(1, 2), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button9 = new wxButton(Panel2, ID_BUTTON9, _("7"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON9"));
+    GridBagSizer3->Add(Button9, wxGBPosition(1, 3), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button10 = new wxButton(Panel2, ID_BUTTON10, _("8"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON10"));
+    GridBagSizer3->Add(Button10, wxGBPosition(1, 4), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button11 = new wxButton(Panel2, ID_BUTTON11, _("9"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON11"));
+    GridBagSizer3->Add(Button11, wxGBPosition(1, 5), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Button12 = new wxButton(Panel2, ID_BUTTON12, _("/"), wxDefaultPosition, wxSize(50,30), 0, wxDefaultValidator, _T("ID_BUTTON12"));
+    GridBagSizer3->Add(Button12, wxGBPosition(1, 6), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Panel2->SetSizer(GridBagSizer3);
+    GridBagSizer3->Fit(Panel2);
+    GridBagSizer3->SetSizeHints(Panel2);
+    GridBagSizer2->Add(Panel2, wxGBPosition(0, 0), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    GridBagSizer1->Add(GridBagSizer2, wxGBPosition(1, 0), wxDefaultSpan, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    SetSizer(GridBagSizer1);
     MenuBar1 = new wxMenuBar();
     Menu1 = new wxMenu();
     MenuItem1 = new wxMenuItem(Menu1, idMenuQuit, _("Quit\tAlt-F4"), _("Quit the application"), wxITEM_NORMAL);
@@ -78,6 +139,8 @@ Scientific_CalculatorFrame::Scientific_CalculatorFrame(wxWindow* parent,wxWindow
     StatusBar1->SetFieldsCount(1,__wxStatusBarWidths_1);
     StatusBar1->SetStatusStyles(1,__wxStatusBarStyles_1);
     SetStatusBar(StatusBar1);
+    GridBagSizer1->Fit(this);
+    GridBagSizer1->SetSizeHints(this);
 
     Connect(idMenuQuit,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&Scientific_CalculatorFrame::OnQuit);
     Connect(idMenuAbout,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&Scientific_CalculatorFrame::OnAbout);
--- Scientific_CalculatorMain.h
+++ Scientific_CalculatorMain.h
@@ -11,7 +11,12 @@
 #define SCIENTIFIC_CALCULATORMAIN_H
 
 //(*Headers(Scientific_CalculatorFrame)
+#include <wx/sizer.h>
 #include <wx/menu.h>
+#include <wx/textctrl.h>
+#include <wx/panel.h>
+#include <wx/gbsizer.h>
+#include <wx/button.h>
 #include <wx/frame.h>
 #include <wx/statusbr.h>
 //*)
@@ -31,13 +36,43 @@ class Scientific_CalculatorFrame: public wxFrame
         //*)
 
         //(*Identifiers(Scientific_CalculatorFrame)
+        static const long ID_TEXTCTRL1;
+        static const long ID_PANEL1;
+        static const long ID_BUTTON1;
+        static const long ID_BUTTON2;
+        static const long ID_BUTTON3;
+        static const long ID_BUTTON4;
+        static const long ID_BUTTON5;
+        static const long ID_BUTTON6;
+        static const long ID_BUTTON7;
+        static const long ID_BUTTON8;
+        static const long ID_BUTTON9;
+        static const long ID_BUTTON10;
+        static const long ID_BUTTON11;
+        static const long ID_BUTTON12;
+        static const long ID_PANEL2;
         static const long idMenuQuit;
         static const long idMenuAbout;
         static const long ID_STATUSBAR1;
         //*)
 
         //(*Declarations(Scientific_CalculatorFrame)
+        wxButton* Button4;
+        wxButton* Button1;
+        wxPanel* Panel1;
+        wxButton* Button2;
+        wxButton* Button6;
+        wxButton* Button10;
+        wxButton* Button11;
+        wxButton* Button5;
+        wxButton* Button3;
+        wxButton* Button7;
         wxStatusBar* StatusBar1;
+        wxButton* Button9;
+        wxTextCtrl* TextCtrl1;
+        wxPanel* Panel2;
+        wxButton* Button8;
+        wxButton* Button12;
         //*)
 
         DECLARE_EVENT_TABLE()
